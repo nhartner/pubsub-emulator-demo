fc8bec06514ed43fdc88b1f81314badf770811b5
Neil Hartner <nhartner@tzero.com>
2019-05-07 13:23:45 -0600

    use test-containers to bring up pubsub emulator

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # pubsub-emulator-demo
-Example Spring project showing how to use GCP emulator in a test.
-
-Before running, you must install and start up GCP pub sub emulator using the instructions here: https://cloud.google.com/pubsub/docs/emulator
+Example Spring project showing how to use launch GCP emulator and test spring GCP pubsub in a test.
 
 Key points:
+- test-containers library is used to launch google-sdk docker container and start pubsub emulator inside it
 - Tests are overridden to hit emulator via `src/test/resources/test.properties` by setting `spring.cloud.gcp.pubsub-emulator-host: localhost:8085` (default emulator port)
-- `TestPubSubConfig` is used to create a topic and subscription in the emulator. This is using GCP SDK directly, not Spring pubsub, to create the resources. Alternatively, the topic and subscription could be created manually using the Python GCP CLI (see https://cloud.google.com/pubsub/docs/emulator)
-- `TestApplication` is used as a hack to force Spring to run TestPubSubConfig initialization before loading the Spring Boot app. This is necessary so that the topic and subscription get created before the Spring Boot application loads and tries to initialize PubSubTemplate and connect to the subscription. This hack isn't necessary if the topic and subscription are created ahead of time manually using the above mentioned Python GCP CLI.
+- `TestPubSubConfig` is used to bring up pubsub emulator inside a docker container, create a topic and subscription in the emulator. This is using GCP SDK directly, not Spring pubsub, to create the resources. 
--- src/test/java/nhartner/demo/pubsub/TestPubSubConfig.java
+++ src/test/java/nhartner/demo/pubsub/TestPubSubConfig.java
@@ -6,9 +6,13 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.gcp.autoconfigure.pubsub.GcpPubSubAutoConfiguration;
+import org.springframework.cloud.gcp.autoconfigure.pubsub.GcpPubSubProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -24,17 +28,40 @@ public class TestPubSubConfig {
     private final String topicName;
     private final String subscriptionName;
     private final PubSubResourceGenerator pubSubResourceGenerator;
+    private final GcpPubSubProperties gcpPubSubProperties;
+    private GenericContainer pubsubContainer;
 
-    TestPubSubConfig(PubSubResourceGenerator pubSubResourceGenerator, DemoConfig config) {
+    TestPubSubConfig(PubSubResourceGenerator pubSubResourceGenerator, GcpPubSubProperties gcpPubSubProperties, DemoConfig config) {
         this.pubSubResourceGenerator = pubSubResourceGenerator;
         this.topicName = config.getTopicName();
         this.subscriptionName = config.getSubscriptionName();
+        this.gcpPubSubProperties = gcpPubSubProperties;
     }
 
     @PostConstruct
-    void createTopicAndSub() {
+    void createResources() {
+        int pubsubPort = 8085;
+        pubsubContainer =
+            new FixedHostPortGenericContainer("google/cloud-sdk:latest")
+                .withFixedExposedPort(pubsubPort, pubsubPort)
+                .withExposedPorts(pubsubPort)
+                .withCommand(
+                    "/bin/sh",
+                    "-c",
+                    String.format(
+                        "gcloud beta emulators pubsub start --project %s --host-port=0.0.0.0:%d",
+                        gcpPubSubProperties.getProjectId(), pubsubPort)
+                )
+                .waitingFor(new LogMessageWaitStrategy().withRegEx("(?s).*started.*$"));
+
+        pubsubContainer.start();
         pubSubResourceGenerator.createTopic(topicName);
         pubSubResourceGenerator.createSubscription(topicName, subscriptionName);
+    }
+
+    @PreDestroy
+    void destroyResources() {
+        pubsubContainer.stop();
     }
 
     @Bean
